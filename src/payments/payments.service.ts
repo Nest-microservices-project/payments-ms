539aa4be62082be8f24e8f0a670c3f515916617a
younesKabiriFarah <youneskabiri17@gmial.com>
2024-06-16 16:30:14 +0200

    Microservices for payments implemented

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,89 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { envs } from 'src/config';
+import { PaymentSessionDto } from 'src/dto/payment-session.dto';
+import Stripe from 'stripe';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymentsService');
+
+  // constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+  constructor() {}
+
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100), // 20 dollars = 2000 / 100 = 20.00 // 15.0000
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const session = await this.stripe.checkout.sessions.create({
+      // Enter my order ID here
+      payment_intent_data: {
+        metadata: {
+          orderId: orderId,
+        },
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: envs.stripeSuccessUrl,
+      cancel_url: envs.stripeCancelUrl,
+    });
+
+    // return session;
+    return {
+      cancelUrl: session.cancel_url,
+      successUrl: session.success_url,
+      url: session.url,
+    };
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    // Real
+    const endpointSecret = envs.stripeEndpointSecret;
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+    console.log('---->', event);
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        console.log('-----kaka--------');
+        const chargeSucceeded = event.data.object;
+        // TODO: llamar nuestro microservicio
+        console.log({
+          metadata: chargeSucceeded.metadata,
+          orderId: chargeSucceeded.metadata.orderId,
+        });
+        break;
+
+      default:
+        console.log(`Event ${event.type} not handled`);
+    }
+
+    return res.status(200).json({ sig });
+  }
+}
