fb478b51e3e41f5580db9c369839cc569435142f
younesKabiriFarah <youneskabiri17@gmial.com>
2024-06-30 20:00:24 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,6 @@ async function bootstrap() {
   await app.startAllMicroservices();
   await app.listen(envs.port);
 
-  logger.log(`Payments Microservice running on port ${envs.port}`);
+  logger.log(`Payments Microservice running on port  ${envs.port}`);
 }
 bootstrap();
